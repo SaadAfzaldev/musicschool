a3e01e8c498e933335c4ed20c1412f4bf857f4db
Saad <saaddevtec@gmail.com>
2024-10-23 06:44:29 +0530

    card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@
 import Hero from "./component/ui/hero";
 import Feauterdcourses from "./component/ui/Feautredcourses";
 import ChooseUs from "./component/ui/whychooseus";
-import { InfiniteMovingCards } from "./component/ui/infinite-moving-cards";
-import testimonials from  "../../data/testimonials.json"
+import Testimonials from "./component/ui/musicshcool-testimonials";
+
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Hero/>
       <Feauterdcourses/>
       <ChooseUs/>
-        <InfiniteMovingCards items={testimonials}/>
+      <Testimonials/>
     </main>
 
   );
